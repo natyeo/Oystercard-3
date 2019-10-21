045363526f4d88043301ebfb3e037648c3f75a14
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-21 15:59:20 +0100

    Refactored text for User story3.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,12 +13,13 @@ describe Oystercard do
         expect { subject.top_up(1) }.to change { subject.balance }.by 1
       end
 
-      it "raises an error if the maximum balance is excceeded" do
-        maximum_balance = Oystercard::MAXIMUM_BALANCE
-        subject.top_up(maximum_balance)
-        expect { subject.top_up 1}.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+      context "Over the limit" do
+        it "raises an error if the maximum balance is excceeded" do
+          maximum_balance = Oystercard::MAXIMUM_BALANCE
+          subject.top_up(maximum_balance)
+          expect { subject.top_up 1}.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+        end
       end
-
 
     end
 

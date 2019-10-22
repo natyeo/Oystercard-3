8f57dee504c930dc61cb2e6be9f2457fe422b330
Natalie <49326857+natyeo@users.noreply.github.com>
2019-10-22 15:21:28 +0100

    Step 10 - added deduct fare on touch out functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -16,10 +16,6 @@ MINIMUM_BALANCE = 1
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
   def in_journey?
     @in_use
   end
@@ -30,6 +26,14 @@ MINIMUM_BALANCE = 1
   end
 
   def touch_out
+    deduct(MINIMUM_BALANCE)
     @in_use = false
   end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,18 @@
 require 'oystercard'
 
-
 describe Oystercard do
 
-  context "balance on card" do
+  context "card initialised" do
     it 'has a balance of zero' do
       expect(subject.balance).to eq(0)
     end
 
+    it 'is not in a journey' do
+      expect(subject).to_not be_in_journey
+    end
+  end
+
     describe "#top_up" do
-      it { is_expected.to respond_to(:top_up).with(1).argument }
 
       it "can top up the balance" do
         expect { subject.top_up(1) }.to change { subject.balance }.by 1
@@ -20,22 +23,9 @@ describe Oystercard do
         subject.top_up(maximum_balance)
         expect { subject.top_up 1}.to raise_error "Maximum balance of #{maximum_balance} exceeded"
       end
-
-    describe "#deduct" do
-      it "deducts an amount from the balance" do
-        subject.top_up(20)
-        expect { subject.deduct 3}.to change { subject.balance }.by -3
-      end
-    end
-    end
-  end
-
-  context "is card in use" do
-    it 'is initially not in a journey' do
-      expect(subject).to_not be_in_journey
     end
 
-    context "touching in" do
+  context "touching in" do
     it 'can touch in' do
       subject.top_up(1)
       subject.touch_in
@@ -43,10 +33,11 @@ describe Oystercard do
     end
 
     it 'raises an error when insufficient balance' do
-    expect { subject.touch_in } .to raise_error "insufficient balance"
+      expect { subject.touch_in } .to raise_error "insufficient balance"
+    end
   end
-end
 
+  context "touching out" do
     it 'can touch out' do
       subject.top_up(1)
       subject.touch_in
@@ -54,6 +45,10 @@ end
       expect(subject).not_to be_in_journey
     end
 
+    it 'deducts fare' do
+      subject.top_up(2)
+      subject.touch_in
+      expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_BALANCE)
+    end
   end
-
 end
